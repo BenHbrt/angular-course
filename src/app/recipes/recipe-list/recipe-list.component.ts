6e5214cc4df9a858929990d754d4e8987beb8ece
BenHbrt <benherbert1983@googlemail.com>
2023-04-07 19:05:35 +0200

    Section 6 -Communicating between components1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,8 +8,13 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
   recipes: Recipe[] = [
     new Recipe("A Test Recipe", "This is simply a test", "https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971556_960_720.jpg"),
     new Recipe("A Test Recipe 2", "This is simply a test 2", "https://cdn.pixabay.com/photo/2017/03/17/10/29/breakfast-2151201_960_720.jpg")
   ]
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
